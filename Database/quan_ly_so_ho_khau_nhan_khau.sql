828cd1a8f89add06916233f6714397e4b9e60e19
thanhnx <thanhlgbg1209@gmail.com>
2022-11-01 10:18:06 +0700

    11/1

---
+++ Database/quan_ly_so_ho_khau_nhan_khau.sql
@@ -0,0 +1,72 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: quan_ly_so_ho_khau
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `nhan_khau`
+--
+
+DROP TABLE IF EXISTS `nhan_khau`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nhan_khau` (
+  `ID` int NOT NULL,
+  `soCMT_CCCD` int NOT NULL,
+  `hoTen` varchar(100) NOT NULL,
+  `ngaySinh` date NOT NULL,
+  `gioiTinh` varchar(100) NOT NULL,
+  `noiSinh` varchar(100) NOT NULL DEFAULT '',
+  `nguyenQuan` varchar(100) NOT NULL,
+  `danToc` varchar(100) NOT NULL,
+  `quocTich` varchar(100) NOT NULL,
+  `soHoChieu` varchar(100) DEFAULT NULL,
+  `noiThuongTru` varchar(100) NOT NULL,
+  `diaChiHienTai` varchar(100) NOT NULL,
+  `tonGiao` varchar(100) NOT NULL,
+  `ghiChu` varchar(100) NOT NULL,
+  `ngayTao` date NOT NULL,
+  `idNguoiTao` int NOT NULL,
+  `ngayXoa` date DEFAULT NULL,
+  `idNguoiXoa` int DEFAULT NULL,
+  `lyDoXoa` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `idNguoiTao` (`idNguoiTao`,`idNguoiXoa`),
+  KEY `idNguoiXoa` (`idNguoiXoa`),
+  CONSTRAINT `nhan_khau_ibfk_1` FOREIGN KEY (`idNguoiTao`) REFERENCES `users` (`id`),
+  CONSTRAINT `nhan_khau_ibfk_2` FOREIGN KEY (`idNguoiXoa`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nhan_khau`
+--
+
+LOCK TABLES `nhan_khau` WRITE;
+/*!40000 ALTER TABLE `nhan_khau` DISABLE KEYS */;
+INSERT INTO `nhan_khau` VALUES (1,12345,'Ngoc','2002-09-12','Nam','BacGiang','BacGiang','Kinh','Viet Nam','123','hoho','haha','Khong','hohooh','2002-09-12',1,'2002-09-12',1,'asdf'),(2,12345,'Thanh','2002-09-12','Nam','BacGiang','BacGiang','Kinh','Viet Nam','123','hoho','haha','Khong','hohooh','2002-09-12',1,'2002-09-12',1,'asdf'),(3,12345,'Nam','2002-09-12','Nam','BacGiang','BacGiang','Kinh','Viet Nam','123','hoho','haha','Khong','hohooh','2002-09-12',1,'2002-09-12',1,'asdf'),(4,12345,'Hai','2002-09-12','Nam','BacGiang','BacGiang','Kinh','Viet Nam','123','hoho','haha','Khong','hohooh','2002-09-12',1,'2002-09-12',1,'asdf'),(5,12345,'Duc','2002-09-12','Nam','BacGiang','BacGiang','Kinh','Viet Nam','123','hoho','haha','Khong','hohooh','2002-09-12',1,'2002-09-12',1,'asdf');
+/*!40000 ALTER TABLE `nhan_khau` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-01 10:15:54
---
+++ Database/quan_ly_so_ho_khau_thanh_vien_cua_ho.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: quan_ly_so_ho_khau
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `thanh_vien_cua_ho`
+--
+
+DROP TABLE IF EXISTS `thanh_vien_cua_ho`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `thanh_vien_cua_ho` (
+  `idNhanKhau` int NOT NULL,
+  `idHoKhau` int NOT NULL,
+  `quanHeVoiChuHo` varchar(100) NOT NULL,
+  KEY `idNhanKhau` (`idNhanKhau`,`idHoKhau`),
+  KEY `idHoKhau` (`idHoKhau`),
+  CONSTRAINT `thanh_vien_cua_ho_ibfk_1` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`),
+  CONSTRAINT `thanh_vien_cua_ho_ibfk_2` FOREIGN KEY (`idHoKhau`) REFERENCES `ho_khau` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `thanh_vien_cua_ho`
+--
+
+LOCK TABLES `thanh_vien_cua_ho` WRITE;
+/*!40000 ALTER TABLE `thanh_vien_cua_ho` DISABLE KEYS */;
+/*!40000 ALTER TABLE `thanh_vien_cua_ho` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-01 10:15:54
